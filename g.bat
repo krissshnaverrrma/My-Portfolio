4dd404da4d8b5f124f01dc562011732a6ac9d17c
Krishna Verma <krishna1290verma@gmail.com>
2026-02-14 22:24:43 +0530

    commit

--- g.bat
+++ g.bat
@@ -1,9 +1,8 @@
 @echo off
-:menu
 cls
-echo      GIT AUTOMATION MANAGER
+echo GIT Command Line Interface
 echo 1. [SAVE]  Add + Commit + Push
-echo 2. [PULL]  Pull changes from Remote
+echo 2. [PULL]  Pull Changes from Remote
 echo 3. [CHECK] Git Status
 echo 4. [LOG]   View Recent History
 echo 5. Exit
@@ -13,38 +12,43 @@ if "%choice%"=="2" goto pull
 if "%choice%"=="3" goto status
 if "%choice%"=="4" goto log
 if "%choice%"=="5" exit
-goto menu
-
+exit
 :save
 echo.
-echo --- STAGING ALL CHANGES ---
+echo STAGING ALL CHANGES 
 git add .
 echo.
 set /p msg="Enter Commit Message (Press Enter for 'Auto Update'): "
 if "%msg%"=="" set msg=Auto Update
 echo.
-echo --- COMMITTING ---
+echo COMMITTING 
 git commit -m "%msg%"
 echo.
-echo --- PUSHING TO REMOTE ---
+echo PUSHING TO REMOTE 
 git push
 echo.
-echo ✅ Done!
-pause
-goto menu
+echo ✅ Done! Closing in 3 Seconds
+timeout /t 3
+exit
 :pull
 echo.
-echo --- PULLING LATEST CHANGES ---
+echo PULLING LATEST CHANGES 
 git pull
-pause
-goto menu
+echo.
+echo ✅ Done! Closing in 3 Seconds
+timeout /t 3
+exit
 :status
 echo.
 git status
+echo.
+echo Press Any Key to Close
 pause
-goto menu
+exit
 :log
 echo.
 git log --oneline --graph --decorate -n 10
+echo.
+echo Press Any Key to Close
 pause
-goto menu
+exit
